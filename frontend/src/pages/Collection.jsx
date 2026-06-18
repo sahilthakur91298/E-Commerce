773b2116e0e6bb4bbacdf6bc9ef3ad6ac37513cd
Sahil Thakur <sahilthakur91298@gmail.com>
2026-06-18 20:21:34 +0530

    Implement search functionality and sorting options in Collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,11 +6,12 @@ import ProductItem from '../components/ProductItem'
 
 const Collection = () => {
 
-  const {products} = useContext(ShopContext)
+  const {products,search,showSearch} = useContext(ShopContext)
   const [showFilter, setShowFilter] = useState(false)
   const [filterProducts, setFilterProducts] = useState([])
   const [category, setCategory] = useState([])
   const [subCategory, setSubCategory] = useState([])
+  const [sortType, setSortType] = useState('relevent')
 
   const toggleCategory = (e)=>{
       if(category.includes(e.target.value)){
@@ -34,22 +35,46 @@ const Collection = () => {
 
     let productsCopy = products.slice();
 
+    if(showSearch && search){
+      productsCopy = productsCopy.filter((item) =>item.name.toLowerCase().includes(search.toLowerCase())) 
+    }
+
     if(category.length>0){
       productsCopy = productsCopy.filter(item=> category.includes(item.category))
+    }
+    if(subCategory.length>0){
+      productsCopy = productsCopy.filter(item=>subCategory.includes(item.subCategory))
     }
 
     setFilterProducts(productsCopy)
   }
 
-  useEffect(()=>{
-    setFilterProducts(products)
-  }, [])
-  
+  const sortProducts = ()=>{
+
+    let fpCopy = filterProducts.slice()
+
+    switch(sortType){
+      case'low-high' : 
+        setFilterProducts(fpCopy.sort((a,b)=>a.price - b.price))
+        break;
+
+      case'high-low' :
+        setFilterProducts(fpCopy.sort((a,b)=>b.price - a.price))
+        break;
+
+        default:
+          applyFilter();
+          break;
+    }
+  }
+    
   useEffect(()=>{
       applyFilter()
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
-
+  useEffect(()=>{
+    sortProducts()
+  },[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -94,7 +119,7 @@ const Collection = () => {
       <div className='flex-1'>
         <div className='flex justify-between text-base sm:text-2xl mb-4'>
           <Title text1={'ALL'} text2={'COLLECTIONS'}/>
-          <select className='border-2 border-gray-300 text-sm px-2'>
+          <select onChange={(e)=>{setSortType(e.target.value)}} className='border-2 border-gray-300 text-sm px-2'>
             <option value='relevent'>Sort by: Relevent</option>
             <option value='low-high'>Sort by: Low to High</option>
             <option value='high-low'>Sort by: High to Low</option>
